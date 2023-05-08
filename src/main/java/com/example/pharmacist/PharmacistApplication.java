7e969027d1bbce3fe22eecedc9a68c0fbb2a076b
sujith-cdlm <sujithcdlm@gmail.com>
2023-05-08 21:35:12 +0530

    initial commit

---
+++ src/main/java/com/example/pharmacist/PharmacistApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pharmacist/PharmacistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
